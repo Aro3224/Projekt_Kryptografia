16ee8dc43a387487dd0f3d480e59f2d991799418
Arkadiusz Zwolenik <arek.zwolenik@o2.pl>
2024-10-21 13:05:21 +0200

    Wpisywanie danych bez algorytmu

--- KryptogrAPPia/KryptogrAPPia/Pages/Polifalfabet.razor.cs
+++ KryptogrAPPia/KryptogrAPPia/Pages/Polifalfabet.razor.cs
@@ -2,6 +2,22 @@ namespace KryptogrAPPia.Pages
 {
     public partial class Polifalfabet
     {
+        private string OriginalText { get; set; }
 
+        private int Key { get; set; }
+        private string EncryptedText { get; set; }
+
+        private string DecryptedText { get; set; }
+
+        public string EncryptPoli(string text)
+        {
+            EncryptedText = text;
+            return EncryptedText;
+        }
+
+        private void DecryptPoli()
+        {
+            DecryptedText = EncryptedText;
+        }
     }
 }
