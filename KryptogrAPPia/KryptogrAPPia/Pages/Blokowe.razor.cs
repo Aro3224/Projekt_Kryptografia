227709f114afecb7a0a2faac94e08ab4317dd6bf
Arkadiusz Zwolenik <arek.zwolenik@o2.pl>
2024-10-28 17:59:22 +0100

    blokowe - wczytywanie pliku

--- KryptogrAPPia/KryptogrAPPia/Pages/Blokowe.razor.cs
+++ KryptogrAPPia/KryptogrAPPia/Pages/Blokowe.razor.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
+using System.Text;
 
 namespace KryptogrAPPia.Pages
 {
@@ -7,27 +9,35 @@ namespace KryptogrAPPia.Pages
         private string InputText { get; set; }
         private string EncryptionKey { get; set; }
         private string EncryptedText { get; set; }
-        private string ErrorMessage { get; set; }
+        private string ErrorMessage { get; set; } = string.Empty;
 
         private async Task EncryptText()
         {
             try
             {
                 // Walidacja d³ugoœci klucza
-                if (string.IsNullOrEmpty(EncryptionKey) ||
-                    (EncryptionKey.Length != 16 && EncryptionKey.Length != 24 && EncryptionKey.Length != 32))
+                if (string.IsNullOrEmpty(EncryptionKey) || (EncryptionKey.Length != 16 && EncryptionKey.Length != 24 && EncryptionKey.Length != 32))
                 {
                     throw new Exception("Klucz szyfrowania musi mieæ 16, 24 lub 32 znaki.");
                 }
 
                 EncryptedText = await JS.InvokeAsync<string>("encryptAES", InputText, EncryptionKey);
                 Console.WriteLine($"Zaszyfrowany tekst: {EncryptedText}");
-                ErrorMessage = string.Empty;
             }
             catch (Exception ex)
             {
-                ErrorMessage = ex.Message;
-                Console.WriteLine($"B³¹d: {ex.Message}");
+                ErrorMessage = ex.Message; // Zapisz komunikat b³êdu
+            }
+        }
+
+        private async Task LoadFile(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+            if (file != null)
+            {
+                var buffer = new byte[file.Size];
+                await file.OpenReadStream().ReadAsync(buffer);
+                InputText = Encoding.UTF8.GetString(buffer);
             }
         }
     }
